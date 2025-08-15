3638da3d17ba29ebfa4d35cde4e479247c83940d
Labib <labib@clodeo.com>
2025-08-16 06:18:46 +0700

    Initial commit

---
+++ apps/k3mart-ui-storybook/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@k3mart-ui/k3mart-ui-storybook" />
+    </div>
+  );
+}
+
+export default App;
